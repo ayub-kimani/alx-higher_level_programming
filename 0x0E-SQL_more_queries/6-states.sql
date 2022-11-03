b019451ab7f81a32136dd6a348ac9edddebd1140
Ayub Kimani <ayubmbugua7@gmail.com>
2022-11-02 17:15:27 -0700

    Updated task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,4 @@
 -- Script with Query to create database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
-       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-       name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL);
